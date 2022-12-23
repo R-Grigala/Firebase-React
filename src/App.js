cca119f139c25e99223882e8fc47e663e6c8eade
R-Grigala <r.grigalashvili777@gmail.com>
2022-12-24 02:09:34 +0400

    Coding Continue

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState,  } from 'react';
+import { app, database } from './firebaseConfig';
+import { 
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword 
+} from 'firebase/auth';
+import { collection, addDoc } from 'firebase/firestore';
 
 function App() {
+
+  const [data, setData] = useState({
+    name : '',
+    email: '',
+    password: ''
+  })
+
+  const auth = getAuth();
+
+  const handleInputs = (event) => {
+    let inputs = {[event.target.name] : event.target.value}
+
+    setData({...data, ...inputs})
+  }
+
+  const handleSubmit = () => {
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <input 
+        placeholder="Name" 
+        name="name" 
+        type="text"
+        className="input-fields"
+        onChange={event => handleInputs(event)}
+      />
+      <input 
+        placeholder="Email" 
+        name="email" 
+        type="email"
+        className="input-fields"
+        onChange={event => handleInputs(event)}
+        />
+      <input 
+        placeholder="Password" 
+        name="password" 
+        type="password"
+        className="input-fields"
+        onChange={event => handleInputs(event)}
+        />
+
+      <button onClick={handleSubmit}>Add data</button>
     </div>
   );
 }
